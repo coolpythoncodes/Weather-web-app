c40ec918cb42b4163643faf4c309ff18f22dd69c
coolpythoncodes <coolpythoncodes@gmail.com>
2021-04-16 09:52:14 +0100

    added previous search history section

--- main.js
+++ main.js
@@ -89,82 +89,152 @@ const setQuery = (e) => {
 };
 
 const getLocationWeather = async function () {
-try {
-    const currentLocationData = await fetch(
-		`${api.baseUrl}?lat=${lat}&lon=${long}&units=metric&appid=${api.key}`
-	).then((result) => result.json());
-	city.innerText = currentLocationData.name;
-
-	temperature.innerHTML = `<h1>${Math.round(
-		currentLocationData.main.temp
-	)}<span>&#176;</span>C</h1>`;
-
-	weatherImage.src = `http://openweathermap.org/img/wn/${currentLocationData.weather[0].icon}.png`;
-
-	weatherCondition.innerText = currentLocationData.weather[0].description;
-
-	// wind weatherData
-	wind.innerText = "Wind";
-	windSpeed.innerText = `${currentLocationData.wind.speed}m/s`;
-
-	// Humidity data
-	humidityText.innerText = "Humidity";
-	humidity.innerText = `${currentLocationData.main.humidity}%`;
-
-	// Cloud data
-	cloudy.innerText = "Cloudy";
-	cloud.innerText = `${currentLocationData.clouds.all}%`;
-
-	// time data
-	time.innerText = `${day}, ${date} ${month} ${year}`;
-} catch (error) {
-    city.innerText = 'Could not get your current location';
-
-}
-};
-
-const getWeatherData = async function (query) {
 	try {
-		const weatherData = await fetch(
-			`${api.baseUrl}?q=${query}&units=metric&appid=${api.key}`
+		const currentLocationData = await fetch(
+			`${api.baseUrl}?lat=${lat}&lon=${long}&units=metric&appid=${api.key}`
 		).then((result) => result.json());
-
-        clearData()
-
-		weatherHeading.innerText = "Weather Details";
-
-		city.innerText = weatherData.name;
+		city.innerText = currentLocationData.name;
 
 		temperature.innerHTML = `<h1>${Math.round(
-			weatherData.main.temp
+			currentLocationData.main.temp
 		)}<span>&#176;</span>C</h1>`;
 
-        weatherImage.style.display = 'block'
-		weatherImage.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+		weatherImage.src = `http://openweathermap.org/img/wn/${currentLocationData.weather[0].icon}.png`;
 
-		weatherCondition.innerText = weatherData.weather[0].description;
+		weatherCondition.innerText = currentLocationData.weather[0].description;
 
 		// wind weatherData
 		wind.innerText = "Wind";
-		windSpeed.innerText = `${weatherData.wind.speed}m/s`;
+		windSpeed.innerText = `${currentLocationData.wind.speed}m/s`;
 
 		// Humidity data
 		humidityText.innerText = "Humidity";
-		humidity.innerText = `${weatherData.main.humidity}%`;
+		humidity.innerText = `${currentLocationData.main.humidity}%`;
 
 		// Cloud data
 		cloudy.innerText = "Cloudy";
-		cloud.innerText = `${weatherData.clouds.all}%`;
+		cloud.innerText = `${currentLocationData.clouds.all}%`;
 
-		// Time data
+		// time data
 		time.innerText = `${day}, ${date} ${month} ${year}`;
 	} catch (error) {
+		city.innerText = "Could not get your current location";
+	}
+};
+
+let history = [];
+const list_of_previous = document.querySelector("#previous");
+
+const getWeatherData = async function (query) {
+	try {
+		const rawData = await fetch(
+			`${api.baseUrl}?q=${query}&units=metric&appid=${api.key}`
+		);
+        const weatherData = await rawData.json();
+        if (weatherData.cod === "404") throw Error;
+
+		clearData();
+
+        console.log(weatherData)
+        if (weatherData.name) {
+            
+            if (!history.includes(weatherData.name)) {
+                if (history.length < 3) {
+                    history.push(weatherData.name);
+                    console.log("doesn't have name")
+                    // list_of_previous.appendChild(previous_item);
+                    const previous_item = document.createElement("li");
+                    // history.map((value) => {
+                    previous_item.textContent = weatherData.name;
+                    previous_item.setAttribute("id", weatherData.name);
+                    previous_item.addEventListener("click", () => {
+                        getWeatherData(weatherData.name);
+                    });
+                    list_of_previous.appendChild(previous_item)
+                } else {
+                    history.shift();
+                    history.push(weatherData.name);
+                    list_of_previous.removeChild(list_of_previous.getElementsByTagName('li')[0])
+                    const previous_item = document.createElement("li");
+                    previous_item.textContent = weatherData.name;
+                    previous_item.setAttribute("id", weatherData.name);
+                    previous_item.addEventListener("click", () => {
+                        getWeatherData(weatherData.name);
+                    });
+                    list_of_previous.appendChild(previous_item)
+                }
+                // });
+            }
+            // previous_items.textContent()
+            console.log(history);
+    
+            weatherHeading.innerText = "Weather Details";
+    
+            city.innerText = weatherData.name;
+    
+            temperature.innerHTML = `<h1>${Math.round(
+                weatherData.main?.temp
+            )}<span>&#176;</span>C</h1>`;
+    
+            weatherImage.style.display = "block";
+            weatherImage.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+    
+            weatherCondition.innerText = weatherData.weather[0].description;
+    
+            // wind weatherData
+            wind.innerText = "Wind";
+            windSpeed.innerText = `${weatherData.wind.speed}m/s`;
+    
+            // Humidity data
+            humidityText.innerText = "Humidity";
+            humidity.innerText = `${weatherData.main.humidity}%`;
+    
+            // Cloud data
+            cloudy.innerText = "Cloudy";
+            cloud.innerText = `${weatherData?.clouds.all}%`;
+    
+            // Time data
+            time.innerText = `${day}, ${date} ${month} ${year}`;
+        }
+
+	} catch (error) {
+		clearData();
 		city.innerText = "Location not found";
 		weatherHeading.innerText = "Something went wrong";
 		console.log("Opps", error);
 	}
 };
 
-window.addEventListener("load", getLocationWeather);
+// window.addEventListener("load", getLocationWeather);
 
 form.addEventListener("submit", setQuery);
+
+const xyz = () => {
+	weatherHeading.innerText = "Weather Details";
+
+	city.innerText = weatherData.name;
+
+	temperature.innerHTML = `<h1>${Math.round(
+		weatherData.main?.temp
+	)}<span>&#176;</span>C</h1>`;
+
+	weatherImage.style.display = "block";
+	weatherImage.src = `http://openweathermap.org/img/wn/${weatherData?.weather[0]?.icon}.png`;
+
+	weatherCondition.innerText = weatherData?.weather[0]?.description;
+
+	// wind weatherData
+	wind.innerText = "Wind";
+	windSpeed.innerText = `${weatherData?.wind?.speed}m/s`;
+
+	// Humidity data
+	humidityText.innerText = "Humidity";
+	humidity.innerText = `${weatherData?.main?.humidity}%`;
+
+	// Cloud data
+	cloudy.innerText = "Cloudy";
+	cloud.innerText = `${weatherData?.clouds?.all}%`;
+
+	// Time data
+	time.innerText = `${day}, ${date} ${month} ${year}`;
+};
